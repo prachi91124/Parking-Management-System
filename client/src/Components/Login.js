47801ebc99d23b196ad14e678879bcb35993b50f
Prachi Anand <prachianand321@gmail.com>
2024-11-18 15:35:23 +0530

    Login Component Done

---
+++ client/src/Components/Login.js
@@ -0,0 +1,30 @@
+import React from "react";
+import '../Styles/Login.css'
+const Login = () => {
+    return (
+        <div class="login-page">
+    <div class="form-container">
+      <h1>Login</h1>
+      <form action="#" method="POST">
+        <div class="form-group">
+          <label for="email">Email</label>
+          <input type="email" id="email" name="email" placeholder="Enter your email" required/>
+        </div>
+
+        <div class="form-group">
+          <label for="password">Password</label>
+          <input type="password" id="password" name="password" placeholder="Enter your password" required/>
+        </div>
+
+        <button type="submit" class="login-btn">Login</button>
+
+        <p class="signup-link">
+          Don't have an account? <a href="signup.html">Sign Up</a>
+        </p>
+      </form>
+    </div>
+  </div>
+    );
+};
+
+export default Login;
