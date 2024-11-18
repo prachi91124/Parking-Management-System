5e59b90b2c8932b50db4aedb97dbe3645de0b393
Prachi Anand <prachianand321@gmail.com>
2024-11-18 15:36:19 +0530

    App.js modified to display all components created

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './Components/Navbar';
+import Footer from './Components/Footer';
+import Body from './Components/Body';
+import Wallet from './Components/Wallet';
+import SignUp from './Components/SignUp';
+import ParkingSpace from './Components/ParkingSpace';
+import Login from './Components/Login';
+import Booking from './Components/Booking';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+     <Navbar />
+     <Footer />
+     {/* <Body /> */}
+     {/* <Booking /> */}
+     <Login />
+     <ParkingSpace />
+     <SignUp />
+     {/* <Wallet /> */}
     </div>
   );
 }
