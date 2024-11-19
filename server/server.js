1bb441809a5daa957c951aafd2d92abef3201700
Prachi Anand <prachianand321@gmail.com>
2024-11-19 15:50:11 +0530

    Backend setup done

---
+++ server/server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const connectDB = require("./config/dbConnection");
+const dotenv = require("dotenv");
+dotenv.config();
+const app = express();
+const errorHandler = require("./middleware/errorHandler");
+const cors = require("cors");
+
+//if .env file is not accesible
+if (dotenv.error) {
+    console.error("Error loading .env file", dotenv.error);
+}
+
+//To connect to MongoDB
+connectDB();
+
+app.use(express.json());
+app.use(cors());
+
+//error Handling Middleware
+app.use(errorHandler);
+
+//routes
+app.use('/api',require("./routes/userRoutes"));
+
+const port = 6000;
+app.listen(port, () => {
+    console.log(`Booting on http://localhost:${port}`);
+});
