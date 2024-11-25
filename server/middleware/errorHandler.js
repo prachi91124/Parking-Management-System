05f67d6052fa6291bf92a2be87e933b6ef4d0d9c
Prachi Anand <prachianand321@gmail.com>
2024-11-25 10:51:03 +0530

    Modified errorHandler to function properly

--- server/middleware/errorHandler.js
+++ server/middleware/errorHandler.js
@@ -1,50 +1,53 @@
-const {constants} = require('../constants');
+const constants = require('../constants');
 
 const errorHandler = (err,req,res,next) =>{
     const statusCode = res.statusCode ? res.statusCode:500;
 
     switch(statusCode) {
         case constants.BAD_REQUEST:
-            res.json({
+            return res.status(statusCode).json({
                 title:"Bad Request",
                 message: err.message,
                 stackTrace: err.stack
             });
-            break;
         case constants.FORBIDDEN:
-            res.json({
+            return res.status(statusCode).json({
                 title:"Forbidden",
                 message: err.message,
                 stackTrace: err.stack
             });
         case constants.NOT_FOUND:
-            res.json({
+            return res.status(statusCode).json({
                 title:"Not Found",
                 message: err.message,
                 stackTrace: err.stack
             });
         case constants.PAYMENT_REQUIRED:
-            res.json({
+            return res.status(statusCode).json({
                 title:"Payment Required",
                 message: err.message,
                 stackTrace: err.stack
             });
         case constants.SERVER_ERROR:
-            res.json({
+            return res.status(statusCode).json({
                 title:"Server Error",
                 message: err.message,
                 stackTrace: err.stack
             });
         case constants.UNAUTHORIZED:
-            res.json({
+            return res.status(statusCode).json({
                 title:"Unauthorized",
                 message: err.message,
                 stackTrace: err.stack
             });
-            
+        
             default:
                 console.log("No error, all good!");
-                 break;
+                return res.status(500).json({
+                    title: "Internal Server Error",
+                    message:"Something went wrong, please try again later.",
+                    stackTrace: err.stack
+                })
     }
 };
 module.exports = errorHandler;
