a4ed88ef70c58ff68fdf92699b620dd604dec90a
Prachi Anand <prachianand321@gmail.com>
2024-11-25 10:52:26 +0530

    Created loginUser and getUserprofile userRoutes

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const router = express.Router();
+const {validateJwtToken} = require("../middleware/jwtAuthMiddleware");
 
 const{
-    registerUser
+    registerUser,
+    loginUser,
+    getUserProfile
 } = require("../controllers/userController");
 
-router.post("/register",registerUser);
+router.post("/register",registerUser) ;
+
+router.post("/login",loginUser);
+
+router.get("/myaccount",getUserProfile);
 
 module.exports = router;
