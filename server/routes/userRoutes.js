e2489e11508e21cb51350b986a934678ca51d1e2
Prachi Anand <prachianand321@gmail.com>
2024-11-25 10:54:52 +0530

    Merged changes

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-<<<<<<< HEAD
+
 const {validateJwtToken} = require("../middleware/jwtAuthMiddleware");
 
 const{
@@ -14,13 +14,6 @@ router.post("/register",registerUser) ;
 router.post("/login",loginUser);
 
 router.get("/myaccount",getUserProfile);
-=======
 
-const{
-    registerUser
-} = require("../controllers/userController");
-
-router.post("/register",registerUser);
->>>>>>> 2e47a2ba05df21828ac5353a0cbb28b01287f5fd
 
 module.exports = router;
